4496167480a5f0e3f73d062b96c0981a48c5f759
TrueReassembly <cameronpttr@gmail.com>
2021-02-16 14:06:04 +0000

    Finished first CommandDupe thing

--- src/main/java/reportcards/dupecore/Commands/CommandDupe.java
+++ src/main/java/reportcards/dupecore/Commands/CommandDupe.java
@@ -1,9 +1,11 @@
 package reportcards.dupecore.Commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
@@ -20,8 +22,14 @@ public class CommandDupe implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         Player player = (Player) commandSender;
         //player.getInventory().getItemInMainHand()
+        ItemStack heldItem = player.getInventory().getItemInMainHand();
+        if (heldItem.equals(Material.AIR)) {
+            player.sendMessage(ChatColor.RED + "You cannot dupe air! Hold an item in your hand to dupe it!");
+            return false;
+        }
+        player.getInventory().addItem(heldItem);
 
 
-        return false;
+        return true;
     }
 }
